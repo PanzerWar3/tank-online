585559ec54ba2a81634da2b18ad4aeda855cc2d4
PanzerWar3 <95978430+PanzerWar3@users.noreply.github.com>
2022-02-13 17:19:57 +0300

    Add files via upload

--- cd.js
+++ cd.js
@@ -1,23 +1,17 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyA0r1WhNz5f1NYeJj1yhzt8nuim0XCxAHg",
-    authDomain: "tankcraft-e525e.firebaseapp.com",
-    databaseURL: "https://tankcraft-e525e-default-rtdb.firebaseio.com",
-    projectId: "tankcraft-e525e",
-    storageBucket: "tankcraft-e525e.appspot.com",
-    messagingSenderId: "76401928225",
-    appId: "1:76401928225:web:6474dbf17153dafbfd5b60",
-    measurementId: "G-HCMLTCH0V3"
-  };
+ const firebaseConfig = {
+  apiKey: "AIzaSyCoSyhZyfr7Kk89ar3off-zhKPApctfQ84",
+  authDomain: "tank-online-d32e0.firebaseapp.com",
+  databaseURL: "https://tank-online-d32e0-default-rtdb.firebaseio.com",
+  projectId: "tank-online-d32e0",
+  storageBucket: "tank-online-d32e0.appspot.com",
+  messagingSenderId: "325542131458",
+  appId: "1:325542131458:web:72db9c43bd95ab6176f41b",
+  measurementId: "G-BBS8CREFRV"
+};
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+ firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const database = getDatabase();
